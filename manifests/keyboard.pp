e26e2d20600d445471b4c38ca7f456ba509d4395
Eivind Uggedal <eivind@uggedal.com>
2011-11-25 14:15:40 +0100

    Proper keyboard realization.

--- manifests/keyboard.pp
+++ manifests/keyboard.pp
@@ -1,11 +1,16 @@
 class mac::keyboard($layout="us") {
+  package { "keyboard-configuration":
+    ensure => installed,
+  }
+
   file { "/etc/default/keyboard":
     content => template("mac/keyboard.erb"),
     notify  => "realize-keyboard",
+    require => Package["keyboard-configuration"],
   }
 
   exec { "realize-keyboard":
-    command => "udevadm trigger --subsystem-match=input --action=change",
+    command => "setupcon && udevadm trigger --subsystem-match=input --action=change",
     refreshonly => true,
   }
 }
